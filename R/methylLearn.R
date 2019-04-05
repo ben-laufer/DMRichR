7279cb2f9d377095d47a39124119fd7dbaaec225
Ben Laufer <blaufer@ucdavis.edu>
2019-04-05 14:40:57 -0700

    First integration of recursive feature elimination with random forest model

--- R/methylLearn.R
+++ R/methylLearn.R
@@ -1,15 +1,18 @@
 #' methylLearn
-#' @description Performs random forest machine learning with k-fold cross validation on significant DMRs
+#' @description Performs random forest machine learning with repeated k-fold cross validation on significant DMRs to identify the most important DMRs
 #' @param bsseq Smoothed bsseq object
 #' @param regions Genomic ranges object
 #' @param names Ordered sample names
 #' @param groups Ordered test covariate information for each sample
 #' @param k.fold number of folds in the cross-validation
+#' @param repeats number of repeats in the cross-validation
 #' @param ... Additional arguments passed onto randomForest::randomForest()
 #' @return ?
 #' @references \url{https://cran.rstudio.com/web/packages/randomForestExplainer/vignettes/randomForestExplainer.html}
 #' @references \url{https://stats.stackexchange.com/questions/82162/cohens-kappa-in-plain-english}
 #' @references \url{https://explained.ai/rf-importance/}
+#' @references \url{https://machinelearningmastery.com/feature-selection-with-the-caret-r-package/}
+#' @references \url{http://ml-tutorials.kyrcha.info/rfe.html}
 #' @import bsseq
 #' @import tidyverse
 #' @import caret
@@ -19,7 +22,8 @@
 #' @export methylLearn
 methylLearn <- function(data = getMeth(BSseq = bs.filtered.bsseq, regions = sigRegions, type = "smooth", what = "perRegion") %>% as.matrix() %>% t(),
                         groups = bs.filtered.bsseq %>% pData() %>% as_tibble() %>% pull(!!testCovariate),
-                        k.fold = 5,
+                        k.fold = 10,
+                        repeats = 3,
                         ...){
 
 # install(c("randomForest", "randomForestExplainer", "caret"))
@@ -32,6 +36,9 @@ methylLearn <- function(data = getMeth(BSseq = bs.filtered.bsseq, regions = sigR
 # load("GO.RData")
 # load("Blocks.RData")
 
+
+  # Tidy --------------------------------------------------------------------
+
   # Create a column to replace names for variable importantance
   colnames(data) <- cbind(as.data.frame(seqnames(sigRegions)), ranges(sigRegions)) %>%
     as_tibble() %>%
@@ -43,6 +50,8 @@ methylLearn <- function(data = getMeth(BSseq = bs.filtered.bsseq, regions = sigR
   # colnames(groupMatrix) <- "groups"
   # data <- cbind(groupMatrix, data)
   # rm(groups)
+  
+  # Random forest: Standard approach ----------------------------------------
   
   # Random forest cross-valdidation for feature selection  
   feature <- randomForest::rfcv(trainx = data,
@@ -68,23 +77,62 @@ methylLearn <- function(data = getMeth(BSseq = bs.filtered.bsseq, regions = sigR
   explain_forest(forest,
                  interactions = F, # True creates an error
                  data = data) 
-  
-  # 5 fold cross validation random forest model -------------------------------------------------
-  
+
+  # Caret approach: RFE and RF ----------------------------------------------
+
   fitRfModel <- function(data){
     set.seed(5)
-    model <- train(groups ~ .,
+    
+    # Remove highly correlated DMRs 
+    
+    # cor.idx <- data %>%
+    #   cor() %>%
+    #   findCorrelation(cutoff = 0.75,
+    #                   exact = T,
+    #                   names = T)
+    # 
+    # data <- data %>%
+    #   tibble::as_tibble() %>%
+    #   dplyr::select(-cor.idx) %>%
+    #   as.matrix()
+    
+    # Recursive feature elimination
+    # Automatically select a subset of the most predictive features, where a Random Forest algorithm is used on each iteration to evaluate the model. 
+    
+    rfeMe <- data %>%
+      rfe(.,
+          groups,
+          rfeControl = rfeControl(functions = rfFuncs,
+                                  method = "repeatedcv",
+                                  number = k.fold,
+                                  repeats = repeats)
+      )
+    
+    # Summarize
+    print(results)
+    
+    # List top predictors
+    predictors(results)
+    
+    # Plot
+    plot(results, type=c("g", "o"))
+    
+    # Fit rf on selected features
+    model <- train(as.formula(paste("groups", paste(rfeMe$optVariables, collapse = " + "), sep = " ~ ")), # groups ~ .
                    data = data,
                    method = "rf",
-                   trControl = trainControl(method = "cv", 
-                                            number = 5, 
+                   tuneGrid = expand.grid(.mtry = length(rfeMe$optVariables)),
+                   trControl = trainControl(method = "repeatedcv", 
+                                            number = k.fold,
+                                            repeats = repeats,
                                             verboseIter = TRUE,
-                                            returnResamp = "final",#all
-                                            savePredictions = "final", #"all"
+                                            returnResamp = "final", #all
+                                            savePredictions = "final", #all
                                             classProbs = TRUE) 
                    )
     return(model)
   }
+  
   model <- fitRfModel(data)
 
   # Confusion matrix of cross-validated results
@@ -93,33 +141,5 @@ methylLearn <- function(data = getMeth(BSseq = bs.filtered.bsseq, regions = sigR
   
   # Feature selection using variable importance
   varImpList <- varImp(object = model)
-
-
-  # Recursive feature elimination  ------------------------------------------
-
-  # Automatically selecting a subset of the most predictive features, where a Random Forest algorithm is used on each iteration to evaluate the model. 
-  # https://machinelearningmastery.com/feature-selection-with-the-caret-r-package/
-  
-  rfeMe <- function(data){
-    set.seed(5)
-    results <- rfe(data,
-                   groups,
-                   rfeControl = rfeControl(functions = rfFuncs,
-                                           method = "cv",
-                                           number = 5)
-    )
-    return(results)
-  }
-  
-  results <- rfeMe(data)
-  
-  # Summarize
-  results
-  
-  # List top predictors
-  predictors(results)
-  
-  # Plot
-  plot(results, type=c("g", "o"))
   
 }
