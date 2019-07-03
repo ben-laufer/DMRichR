9ef83ef8a452b8fb05fc92024f92ef69c5b9c441
hyeyeon-hwang <hyehwang@ucdavis.edu>
2019-07-03 13:31:53 -0700

    cleaned function, updated documentation

--- R/methylLearn.R
+++ R/methylLearn.R
@@ -1,61 +1,83 @@
 #' methylLearn
-#' @description Performs random forest and suport vector machine feature selection on significant DMRs to identify the most important DMRs
-#' @param bsseq Smoothed bsseq object
-#' @param regions Genomic ranges object
-#' @param groups Ordered test covariate information for each sample
-#' @return A tibble object of ranked and annotated DMRs 
-#' @references \url{https://cran.rstudio.com/web/packages/randomForestExplainer/vignettes/randomForestExplainer.html}
-#' @references \url{https://stats.stackexchange.com/questions/82162/cohens-kappa-in-plain-english}
-#' @references \url{https://explained.ai/rf-importance/}
-#' @references \url{https://machinelearningmastery.com/feature-selection-with-the-caret-r-package/}
-#' @references \url{http://ml-tutorials.kyrcha.info/rfe.html}
+#' @description Performs feature selection on significant DMRs (predictors) based on random forest (RF) and support vector machine (SVM)
+#' algorithms to generate two lists of DMRs ranked by order of importance. Then finds and annotates the DMRs that overlap between the 
+#' top 1% of DMRs in the two DMR ranking lists.
+#' @param bsseq Smoothed bsseq object.
+#' @param regions Genomic ranges object.
+#' @param testCovariate Factor of interest.
+#' @param TxDb TxDb annotation package for genome of interest.
+#' @param annoDb Character specifying OrgDb annotation package for species of interest.
+#' @return A tibble object containing the variable importance ranks and annotations of the most important DMRs, which overlap between the 
+#' top 1% of DMRs in the two DMR ranking lists obtained from feature selection methods using RF and SVM algorithsms.
+#' @references \url{https://www.analyticsvidhya.com/blog/2016/03/select-important-variables-boruta-package/}
 #' @import bsseq
 #' @import tidyverse
+#' @import ChIPseeker
+#' @import Boruta
+#' @import sigFeature
 #' @export methylLearn
+methylLearn <- function(bsseq = bs.filtered.bsseq, 
+                        regions = sigRegions, 
+                        testCovariate = testCovariate, 
+                        TxDb = NA, 
+                        annoDb = NA) {
 
+  cat(glue::glue("[DMRichR] Learning features of DMRs for {testCovariate}", "\t", format(Sys.time(), "%d-%m-%Y %X"), "\n"))
 
-methylLearn <- function(BSseq = bs.filtered.bsseq, regions = sigRegions) {
-  
   # Tidy data ---------------------------------------------------------------
-  
-  data = getMeth(BSseq = bs.filtered.bsseq, regions = sigRegions, type = "smooth", what = "perRegion") %>% as.matrix() %>% t()
-  groups = bs.filtered.bsseq %>% pData() %>% as_tibble() %>% pull(!!testCovariate)
+  data <- getMeth(BSseq = bsseq, regions = regions, type = "smooth", what = "perRegion") %>% 
+    as.matrix() %>% 
+    t()
+  groups <- bsseq %>% 
+    pData() %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::pull(!!testCovariate)
   
   # Create a column to replace names for variable importance
-  colnames(data) <- cbind(as.data.frame(seqnames(sigRegions)), ranges(sigRegions)) %>%
-    as_tibble() %>%
+  colnames(data) <- cbind(as.data.frame(seqnames(regions)), ranges(regions)) %>%
+    dplyr::as_tibble() %>%
     dplyr::select(value,start,end) %>%
     tidyr::unite("bed", c("value","start","end"), sep = ".") %>%
     as.matrix()
   
   # Add "groups" column to "data" tibble 
-  data <- data %>% as_tibble() %>% add_column(groups = groups, .before = 1)
+  data <- data %>% 
+    dplyr::as_tibble() %>% 
+    tibble::add_column(groups = groups, .before = 1)
   
   
   # Random forest variable importance ---------------------------------------
   # using Boruta algorithm in "Boruta" package
-  
   getBorutaRanking <- function() {
+    cat("\n", "Training random forest (RF) model...")
     set.seed(5)
-    borutaTrain <- Boruta(groups ~ ., data = data, doTrace = 2)  
+    borutaTrain <- Boruta(groups ~ ., data = data, doTrace = 0)  
     borutaTrainStats <- attStats(borutaTrain)
-    borutaRanking <- tibble(DMR = rownames(borutaTrainStats), meanImp = borutaTrainStats$meanImp, decision = borutaTrainStats$decision) %>% 
-      arrange(dplyr::desc(meanImp)) %>% 
-      add_column(Ranking = 1:nrow(borutaTrainStats), .before = 1)
+    borutaRanking <- tibble::tibble(DMR = rownames(borutaTrainStats), 
+                                    meanImp = borutaTrainStats$meanImp, 
+                                    decision = borutaTrainStats$decision) %>% 
+      dplyr::arrange(dplyr::desc(meanImp)) %>% 
+      tibble::add_column(Ranking = 1:nrow(borutaTrainStats), .before = 1)
+    cat("Done")
     return(borutaRanking)
   }
   
   
   # Support vector machine variable importance ------------------------------
-  # using RFE & t-statistic in "sigFeature" package
-  
+  # using recursive feature elimination (RFE) algorithm  & t-statistic in "sigFeature" package
   getSigfeatureRanking <- function() {
-    dataMatrix <- data %>% dplyr::select(-groups) %>% as.matrix()
+    cat("\n", "Training rupport vector machine (SVM) model...")
+    dataMatrix <- data %>% 
+      dplyr::select(-groups) %>% 
+      as.matrix()
     set.seed(5)
     sigfeatureObject <- sigFeature(dataMatrix, data$groups) 
-    sigfeatureRanking <- tibble(Ranking = 1:length(sigfeatureObject), DMR = colnames(dataMatrix[, sigfeatureObject]))
+    sigfeatureRanking <- tibble::tibble(Ranking = 1:length(sigfeatureObject), 
+                                        DMR = colnames(dataMatrix[, sigfeatureObject]))
+    cat("Done", "\n")
     return(sigfeatureRanking)
   }  
+  
   
   rfRanking <- getBorutaRanking()
   svmRanking <- getSigfeatureRanking()
@@ -63,50 +85,41 @@ methylLearn <- function(BSseq = bs.filtered.bsseq, regions = sigRegions) {
   
   # Find overlapping DMRs (predictors) --------------------------------------
   # Find overlap between top 1% predictors in RF and SVM variable importance lists
-  numPredictors = ncol(data) - 1 
-  percent1 = ceiling(.01 * numPredictors) 
-  
+  numPredictors <- ncol(data) - 1 
+  percent1 <- ceiling(.01 * numPredictors) 
   if (percent1 < 10) {
     overlappingTopDmrs <- intersect(rfRanking$DMR[1:10], svmRanking$DMR[1:10]) 
-    overlappingTopDmrs_rfRank <- which(rfRanking$DMR %in% overlappingTopDmrs)
-    overlappingTopDmrs_svmRank <- which(svmRanking$DMR %in% overlappingTopDmrs)
   } else {
     overlappingTopDmrs <- intersect(rfRanking$DMR[1:percent1], svmRanking$DMR[1:percent1]) 
-    overlappingTopDmrs_rfRank <- which(rfRanking$DMR %in% overlappingTopDmrs)
-    overlappingTopDmrs_svmRank <- which(svmRanking$DMR %in% overlappingTopDmrs)
   }
+  overlappingTopDmrs_rfRank <- which(rfRanking$DMR %in% overlappingTopDmrs)
+  overlappingTopDmrs_svmRank <- which(svmRanking$DMR %in% overlappingTopDmrs)
+  
   
   # Annotate DMRs -----------------------------------------------------------
-  annotateDmr <- function(dmrList, rfRank, svmRank, filename) {
-    dmrSplitted <- strsplit(dmrList, split = "[.]")
-    toAnnotate <- data.frame(matrix(unlist(dmrSplitted), nrow = length(dmrSplitted), byrow = T))
-    colnames(toAnnotate) <- c("chr", "start", "end")
-    
-    toAnnotate %>% mutate_if(is.factor, as.character) -> toAnnotate
-    
-    toAnnotate <- toAnnotate %>%
+  annotateDmr <- function(dmrList, rfRank, svmRank) {
+    annotatedDmrs <- dmrList %>% 
+      strsplit(., split = "[.]") %>%
+      as.data.frame() %>%
+      t() %>%
+      magrittr::set_colnames(c("chr", "start", "end")) %>%
+      dplyr::as_tibble() %>%
       GenomicRanges::makeGRangesFromDataFrame(ignore.strand = TRUE,
                                               seqnames.field = "chr",
                                               start.field = "start",
                                               end.field = "end") %>%
-      ChIPseeker::annotatePeak(TxDb = TxDb.Hsapiens.UCSC.hg38.knownGene,
-                               annoDb = "org.Hs.eg.db",
+      ChIPseeker::annotatePeak(TxDb = TxDb,
+                               annoDb = annoDb, 
                                overlap = "all") %>%
-      as.data.frame() %>%  
-      add_column(RF.varImp.rank = rfRank, .before = 1) %>%
-      add_column(SVM.varImp.rank = svmRank, .before = 2) %>%
-      select(-c(strand, geneChr, geneStart, geneEnd, geneStrand, geneId, transcriptId, distanceToTSS, ENSEMBL)) 
-    
-    
-    toAnnotate %>% openxlsx::write.xlsx(file = filename)
-    return(toAnnotate)  
-    
+      dplyr::as_tibble() %>%  
+      tibble::add_column(RF.varImp.rank = rfRank, .before = 1) %>%
+      tibble::add_column(SVM.varImp.rank = svmRank, .before = 2) %>%
+      dplyr::select(-c(strand, geneChr, geneStart, geneEnd, geneStrand, geneId, transcriptId, distanceToTSS, ENSEMBL))
+    return(annotatedDmrs)  
   }
   
-  annotateDmr(overlappingTopDmrs, overlappingTopDmrs_rfRank, overlappingTopDmrs_svmRank, "DS_Annotated_Overlapping_Top_DMRs.xlsx")
-  
+  annotateDmr(overlappingTopDmrs, overlappingTopDmrs_rfRank, overlappingTopDmrs_svmRank)
 }
-
 
 
 
