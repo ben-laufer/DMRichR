ben-laufer/DMRichR

commit ad8ce9a159168bd3a97468e1374ae013a4c1e1f8

Dev (#33) Many minor improvements
* Added support for hg19 and mm9
* Added annotations for blocks, where DMReport() now supports blocks and DM.R generates an HTML block report
* Tidying of the annotation pipeline to add gene symbols, percent difference, and directionality to regions of interest, where annotateRegions() was developed from previous helper functions and code in DM.R
* Fix for unnest warning message in globalStats()
* Improvements to parallel processing and added progress bar back to BSmooth()
* Reorganized functions in R folder and removed legacy functions

DESCRIPTION
@@ -1,6 +1,6 @@
 Package: DMRichR
 Title: Enrich your DMR analysis
-Version: 1.2.1
+Version: 1.2.3
 Authors@R: c(
   person("Ben", "Laufer", email = "blaufer@ucdavis.edu", role = c("aut", "cre")),
   person("Charles", "Mordaunt", email = "cemordaunt@ucdavis.edu", role = "ctb"),
@@ -43,5 +43,5 @@ Imports: BiocGenerics,
 License: GNU General Public License v3.0
 Encoding: UTF-8
 LazyData: true
-RoxygenNote: 7.0.2
+RoxygenNote: 7.1.0
 Remotes: vqv/ggbiplot, rstudio/gt, stephenturner/annotables

R/helperFunctions.R
@@ -1,41 +1,3 @@
-#' packageLoad
-#' @description Install and load desired packages
-#' @param packages Character string of desired packages
-#' @import optparse
-#' @export packageLoad
-packageLoad <- function(packages = packages){
-  cat("\n","Checking for BiocManager and helpers...")
-  CRAN <- c("BiocManager", "remotes", "magrittr")
-  new.CRAN.packages <- CRAN[!(CRAN %in% installed.packages()[,"Package"])]
-  if(length(new.CRAN.packages)>0){
-    install.packages(new.CRAN.packages, repos ="https://cloud.r-project.org", quiet = TRUE)
-  }
-  cat("Done")
-  cat("\n", "Loading package management...")
-  stopifnot(suppressMessages(sapply(CRAN, require, character.only = TRUE)))
-  cat("Done")
-  
-  new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
-  if(length(new.packages)>0){
-    cat("Installing missing packages...")
-    new.packages <- packages %>%
-      gsub("ggbiplot", "vqv/ggbiplot", .) %>% 
-      gsub("DMRichR", "ben-laufer/DMRichR", .) %>% 
-      gsub("gt", "rstudio/gt", .) %>%
-      gsub("annotables", "stephenturner/annotables",  .)
-    BiocManager::install(new.packages, ask = FALSE, quiet = TRUE)
-    cat("Done")
-  }
-  cat("\n", "Loading packages...")
-  stopifnot(suppressMessages(sapply(packages, require, character.only = TRUE)))
-  suppressWarnings(BiocManager::valid(fix = TRUE, update = TRUE, ask = FALSE))
-  cat("Done")
-  cat("\n", "Checking for github repository updates...")
-  suppressMessages(BiocManager::install(c("vqv/ggbiplot", "ben-laufer/DMRichR", "rstudio/gt", "stephenturner/annotables")))
-  stopifnot(suppressMessages(sapply(c("ggbiplot", "DMRichR", "gt", "annotables"), require, character.only = TRUE)))
-  cat("Done", "\n")
-}
-
 #' getSmooth
 #' @description Provides individual smoothed methylation values for a \code{GRanges} object using \code{bsseq}
 #' @param bsseq A smoothed \code{bsseq} object
@@ -160,93 +122,6 @@ getBackground <- function(bs = bs.filtered,
         return(background)
 }
 
-#' labelDirection
-#' @description Annotate a \code{GRanges} object containing either DMRs or background regions with directionality of change
-#'  (i.e. hypermethlated or hypomethylated).
-#' @param regions A \code{GRanges} object returned by \code{dmrseq:dmrseq()}.
-#' @return An \code{GRanges} object annotated with directionality of change.
-#' @import GenomicRanges
-#' @export labelDirection
-labelDirection <- function(regions = sigRegions){
-  cat("\n", "Annotating regions with directionality...")
-  external <- regions
-  for (i in 1:length(external)){
-    if(external$stat[i] > 0){
-      external$direction[i] <- "Hypermethylated"
-    }else if(external$stat[i] < 0){
-      external$direction[i] <- "Hypomethylated"
-    }else{
-      stop("Annotation problem")
-    }}
-  cat("Done", "\n")
-  return(external)
-}
-
-#' tidyRegions
-#' @description Tidy DMRs or background regions from \code{dmrseq::dmrseq()} that have been annotated using \code{ChIPseeker}
-#' @param regions A \code{ChIPseeker csAnno} peak object of DMRs or background regions from \code{dmrseq::dmrseq()}
-#' @return A \code{tibble} of annotated regions
-#' @import tidyverse
-#' @export tidyRegions
-tidyRegions <- function(regions = sigRegionsAnno){
-  regions %>% 
-    dplyr::as_tibble() %>%
-    dplyr::select("seqnames", "start", "end", "width", "L",
-                  "beta", "stat", "pval", "qval", "percentDifference",
-                  "annotation", "distanceToTSS", "ENSEMBL", "SYMBOL", "GENENAME") %>%
-    dplyr::rename(CpGs = L,
-                  betaCoefficient = beta, statistic = stat,
-                  "p-value" = pval, "q-value" = qval, difference = percentDifference,
-                  geneSymbol = SYMBOL, gene = GENENAME) %>% 
-    return()
-}
-
-#' DMReport
-#' @description Create an html report of a \code{ChIPseeker csAnno} peak object with genic annotations
-#' @param tidySigRegionsAnno A \code{ChIPseeker csAnno} peak object of DMRs from \code{dmrseq::dmrseq()}
-#'  that has been tidied with \code{tidyDMRs}
-#' @param regions \code{GRanges} object of background regions
-#' @param bsseq Smoothed \code{bsseq} object
-#' @param coverage Numeric of coverage samples were filtered for
-#' @return Saves an html report of DMRs with genic annotations
-#' @import gt
-#' @import tidyverse
-#' @importFrom glue glue
-#' @export DMReport
-DMReport <- function(tidySigRegionsAnno = tidySigRegionsAnno,
-                     regions = regions,
-                     bsseq = bs.filtered.bsseq,
-                     coverage = coverage){
-  cat("\n","Preparing HTML report...")
-  tidySigRegionsAnno %>%
-    dplyr::select(-ENSEMBL, -betaCoefficient, -statistic) %>%
-    dplyr::mutate(difference = difference/100) %>% 
-    gt() %>%
-    tab_header(
-      title = glue::glue("{nrow(tidySigRegionsAnno)} Significant DMRs"),
-      subtitle = glue::glue("{nrow(tidySigRegionsAnno)} Significant DMRs \\
-                         {round(sum(tidySigRegionsAnno$statistic > 0) / nrow(tidySigRegionsAnno), digits = 2)*100}% hypermethylated, \\
-                         {round(sum(tidySigRegionsAnno$statistic < 0) / nrow(tidySigRegionsAnno), digits = 2)*100}% hypomethylated \\
-                         in {length(regions)} background regions \\
-                         from {nrow(bs.filtered)} CpGs assayed at {coverage}x coverage")
-    ) %>% 
-    fmt_number(
-      columns = vars("width", "CpGs"),
-      decimals = 0
-    ) %>% 
-    fmt_scientific(
-      columns = vars("p-value", "q-value"),
-      decimals = 2
-    ) %>%
-    fmt_percent(
-      columns = vars("difference"),
-      drop_trailing_zeros = TRUE
-    ) %>% 
-    as_raw_html(inline_css = TRUE) %>%
-    write("DMReport.html")
-  cat("Done", "\n")
-}
-
 #' manQQ
 #' @description Create manhattan and Quantile-Quantile (Q-Q) plots of \code{ChIPseeker csAnno} peak object with genic annotations using \code{CMplot}
 #' @param peakAnno A \code{ChIPseeker csAnno} peak object of background regions from \code{dmrseq::dmrseq()}
@@ -296,7 +171,6 @@ manQQ <- function(backgroundAnno = backgroundAnno,
 #' @param siRegions A \code{GRanges} object of signficant DMRs returned by \code{dmrseq::dmrseq()}
 #' @param regions A \code{GRanges} object of background regions returned by \code{dmrseq::dmrseq()}
 #' @return Saves external GAT and HOMER folders with bed files into the working directory
-#' @import GenomeInfoDb
 #' @import tidyverse
 #' @importFrom glue glue
 #' @export saveExternal
@@ -309,13 +183,21 @@ saveExternal <- function(sigRegions = sigRegions,
   glue::glue("Preparing regions for external GAT analysis...")
   dir.create("Extra/GAT")
   
+  sigRegions <- sigRegions %>% 
+    dplyr::as_tibble() %>%
+    dplyr::mutate(direction = dplyr::case_when(stat > 0 ~ "Hypermethylated",
+                                               stat < 0 ~ "Hypomethylated"
+                                               )
+                  )
+  
+  regions <- regions %>%
+    dplyr::as_tibble() 
+  
   sigRegions %>%
-    GenomeInfoDb::as.data.frame() %>%
     dplyr::select(seqnames, start, end, direction) %>% 
     DMRichR::df2bed("Extra/GAT/DMRs.bed")
   
   regions %>%
-    GenomeInfoDb::as.data.frame() %>%
     dplyr::select(seqnames, start, end) %>% 
     DMRichR::df2bed("Extra/GAT/background.bed")
   
@@ -323,19 +205,16 @@ saveExternal <- function(sigRegions = sigRegions,
   dir.create("Extra/HOMER")
   
   sigRegions %>%
-    GenomeInfoDb::as.data.frame() %>% 
     dplyr::filter(direction == "Hypermethylated") %>%
     dplyr::select(seqnames, start, end) %>%
     DMRichR::df2bed("Extra/HOMER/DMRs_hyper.bed")
   
   sigRegions %>%
-    GenomeInfoDb::as.data.frame() %>% 
     dplyr::filter(direction == "Hypomethylated") %>%
     dplyr::select(seqnames, start, end) %>%
     DMRichR::df2bed("Extra/HOMER/DMRs_hypo.bed")
   
   regions %>%
-    GenomeInfoDb::as.data.frame() %>%
     dplyr::select(seqnames, start, end) %>% 
     DMRichR::df2bed("Extra/HOMER/background.bed")
   

exec/DM.R
@@ -32,7 +32,7 @@ cat("\n[DMRichR] Processing arguments from script \t\t", format(Sys.time(), "%d-
 
 option_list <- list(
   optparse::make_option(c("-g", "--genome"), type = "character", default = NULL,
-              help = "Choose a genome (hg38, mm10, rn6, rheMac8) [required]"),
+              help = "Choose a genome (hg38, hg19, mm10, mm9, rheMac8, rn6) [required]"),
   optparse::make_option(c("-x", "--coverage"), type = "integer", default = 1,
               help = "Choose a CpG coverage cutoff [default = %default]"),
   optparse::make_option(c("-s", "--perGroup"), type = "double", default = 1,
@@ -49,7 +49,7 @@ option_list <- list(
               help = "Choose covariates to directly adjust [default = NULL]"),
   optparse::make_option(c("-m", "--matchCovariate"), type = "character", default = NULL,
               help = "Choose covariate to balance permutations [default = NULL]"),
-  optparse::make_option(c("-c", "--cores"), type = "integer", default = 8,
+  optparse::make_option(c("-c", "--cores"), type = "integer", default = 20,
               help = "Choose number of cores [default = %default]")
 )
 opt <- optparse::parse_args(optparse::OptionParser(option_list = option_list))
@@ -99,7 +99,9 @@ glue::glue("cores = {cores}")
 cat("\n[DMRichR] Selecting annotation databases \t\t", format(Sys.time(), "%d-%m-%Y %X"), "\n")
 
 packages <- dplyr::case_when(genome == "hg38" ~ c("BSgenome.Hsapiens.UCSC.hg38", "TxDb.Hsapiens.UCSC.hg38.knownGene", "org.Hs.eg.db"),
+                             genome == "hg19" ~ c("BSgenome.Hsapiens.UCSC.hg19", "TxDb.Hsapiens.UCSC.hg19.knownGene", "org.Hs.eg.db"),
                              genome == "mm10" ~ c("BSgenome.Mmusculus.UCSC.mm10", "TxDb.Mmusculus.UCSC.mm10.knownGene", "org.Mm.eg.db"),
+                             genome == "mm9" ~ c("BSgenome.Mmusculus.UCSC.mm9", "TxDb.Mmusculus.UCSC.mm9.knownGene", "org.Mm.eg.db"),
                              genome == "rheMac8" ~ c("BSgenome.Mmulatta.UCSC.rheMac8", "TxDb.Mmulatta.UCSC.rheMac8.refGene", "org.Mmu.eg.db"),
                              genome == "rn6" ~ c("BSgenome.Rnorvegicus.UCSC.rn6", "TxDb.Rnorvegicus.UCSC.rn6.refGene", "org.Rn.eg.db")
                              )
@@ -117,9 +119,17 @@ if(genome == "hg38"){
   goi <- BSgenome.Hsapiens.UCSC.hg38
   TxDb <- TxDb.Hsapiens.UCSC.hg38.knownGene
   annoDb <- "org.Hs.eg.db"
+}else if(genome == "hg19"){
+  goi <- BSgenome.Hsapiens.UCSC.hg19 
+  TxDb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+  annoDb <- "org.Hs.eg.db"
 }else if(genome == "mm10"){
   goi <- BSgenome.Mmusculus.UCSC.mm10
   TxDb <- TxDb.Mmusculus.UCSC.mm10.knownGene
+  annoDb <- "org.Mm.eg.db"
+}else if(genome == "mm9"){
+  goi <- BSgenome.Mmusculus.UCSC.mm9
+  TxDb <- TxDb.Mmusculus.UCSC.mm9.knownGene
   annoDb <- "org.Mm.eg.db"
 }else if(genome == "rheMac8"){
   goi <- BSgenome.Mmulatta.UCSC.rheMac8
@@ -130,7 +140,7 @@ if(genome == "hg38"){
   TxDb <- TxDb.Rnorvegicus.UCSC.rn6.refGene
   annoDb <- "org.Rn.eg.db"
 }else{
-  stop(glue("{genome} is not supported, please choose either hg38, mm10, rheMac8, or rn6 [Case Sensitive]"))
+  stop(glue("{genome} is not supported, please choose either hg38, hg19, mm10, mm9, rheMac8, or rn6 [Case Sensitive]"))
 }
 
 # Load and process samples ------------------------------------------------
@@ -177,26 +187,15 @@ getBackground(bs.filtered,
 cat("\n[DMRichR] Testing for DMRs with dmrseq \t\t\t", format(Sys.time(), "%d-%m-%Y %X"), "\n")
 start_time <- Sys.time()
 
-# Reproducible permutations (change and record seed for different datasets to avoid any potential random bias)
-set.seed(5)
-#.Random.seed
-
-# More cores increases smoothing time but decreases scoring time, so this is my attempt at balancing it
-glue::glue("Determining parallelization...") 
-if(cores >= 2){
-  BPPARAM <- BiocParallel::MulticoreParam(workers = ceiling(cores/2))
-}else if(cores < 2){
-  BPPARAM <- BiocParallel::MulticoreParam(workers = 1)
-}
-BiocParallel::register(BPPARAM)
-
 regions <- dmrseq::dmrseq(bs = bs.filtered,
                           cutoff = cutoff,
                           minNumRegion = minCpGs,
                           maxPerms = maxPerms,
                           testCovariate = testCovariate,
                           adjustCovariate = adjustCovariate,
-                          matchCovariate = matchCovariate)
+                          matchCovariate = matchCovariate,
+                          BPPARAM = BiocParallel::MulticoreParam(workers = cores)
+                          )
 
 glue::glue("Selecting significant DMRs...", "\n")
 if(sum(regions$qval < 0.05) < 100 & sum(regions$pval < 0.05) != 0){
@@ -207,24 +206,10 @@ if(sum(regions$qval < 0.05) < 100 & sum(regions$pval < 0.05) != 0){
   stop(glue::glue("No significant DMRs detected in {length(regions)} background regions"))
   }
 
-glue::glue("Calculating average percent differences...") 
-regions$percentDifference <- round(regions$beta/pi * 100)
-sigRegions$percentDifference <- round(sigRegions$beta/pi *100)
-
-glue::glue("Adding directionality to DMRs...")
-sigRegions <- sigRegions %>%
-  labelDirection()
-
-glue::glue("Adding directionality to background regions...")
-regions <- regions %>%
-  labelDirection()
-
 glue::glue("Exporting DMR and background region information...")
 dir.create("DMRs")
-gr2bed(sigRegions,
-       "DMRs/DMRs.bed")
-gr2bed(regions,
-       "DMRs/backgroundRegions.bed")
+gr2bed(sigRegions, "DMRs/DMRs.bed")
+gr2bed(regions, "DMRs/backgroundRegions.bed")
 
 saveExternal(sigRegions = sigRegions,
              regions = regions)
@@ -252,8 +237,8 @@ cat("\n[DMRichR] Smoothing individual methylation values \t\t", format(Sys.time(
 start_time <- Sys.time()
 
 bs.filtered.bsseq <- BSmooth(bs.filtered,
-                             BPPARAM = MulticoreParam(workers = ceiling(cores/2),
-                                                      progressbar = FALSE)
+                             BPPARAM = MulticoreParam(workers = cores,
+                                                      progressbar = TRUE)
                              )
 
 # Drop chrY in Rat only due to poor quality (some CpGs in females map to Y)
@@ -345,7 +330,7 @@ bs.filtered.bsseq %>%
                   width = 11,
                   height = 8.5)
 
-if(genome == "hg38" | genome == "mm10" | genome == "rn6"){
+if(genome == "hg38" | genome == "hg19" | genome == "mm10" | genome == "mm9" | genome == "rn6"){
   bs.filtered.bsseq %>%
     CGiPCA(genome = genome, 
            group = bs.filtered.bsseq %>%
@@ -381,7 +366,7 @@ sigRegions %>%
 
 # CpG and genic annotations -----------------------------------------------
 
-if(genome == "hg38" | genome == "mm10" | genome == "rn6"){
+if(genome == "hg38" | genome == "hg19" | genome == "mm10" | genome == "mm9" | genome == "rn6"){
   annotateCpGs(sigRegions = sigRegions,
                regions = regions,
                genome = genome,
@@ -403,32 +388,24 @@ if(genome == "hg38" | genome == "mm10" | genome == "rn6"){
                     height = 11)
 }
 
-# ChIPseeker --------------------------------------------------------------
+
+# Gene symbol annotations -------------------------------------------------
 
 cat("\n[DMRichR] Annotating DMRs with gene symbols \t\t", format(Sys.time(), "%d-%m-%Y %X"), "\n")
-sigRegionsAnno <- sigRegions %>%
-  ChIPseeker::annotatePeak(TxDb = TxDb,
-                           annoDb = annoDb,
-                           overlap = "all")
-                               
-glue::glue("Annotating background regions with gene symbols...")
-regionsAnno <- regions %>%
-  ChIPseeker::annotatePeak(TxDb = TxDb,
-                           annoDb = annoDb,
-                           overlap = "all")
 
-glue::glue("Saving gene annotations...")
-
-sigRegionsAnno %>%
-  tidyRegions() %T>%
-  DMReport(.,
-           regions = regions,
+sigRegions %>%
+  annotateRegions(TxDb = TxDb,
+                  annoDb = annoDb) %T>%
+  DMReport(regions = regions,
            bsseq = bs.filtered.bsseq,
-           coverage = coverage) %>% 
+           coverage = coverage,
+           name = "DMReport") %>% 
   openxlsx::write.xlsx(file = "DMRs/DMRs_annotated.xlsx")
 
-regionsAnno %>%
-  tidyRegions() %>% 
+glue::glue("Annotating background regions with gene symbols...")
+regions %>%
+  annotateRegions(TxDb = TxDb,
+                  annoDb = annoDb)
   openxlsx::write.xlsx(file = "DMRs/background_annotated.xlsx")
   
 # Manhattan and Q-Q plots -------------------------------------------------
@@ -445,8 +422,9 @@ dir.create("Ontologies")
 glue::glue("Running enrichR")
 library(enrichR) # Needed or else "EnrichR website not responding"
 #dbs <- listEnrichrDbs()
-sigRegionsAnno %>%
-  tidyRegions() %>% 
+sigRegions %>%
+  annotateRegions(TxDb = TxDb,
+                  annoDb = annoDb) %>%  
   dplyr::select(geneSymbol) %>%
   purrr::flatten() %>%
   enrichR::enrichr(c("GO_Biological_Process_2018",
@@ -466,7 +444,7 @@ sigRegionsAnno %>%
                   width = 12)
 
 glue::glue("Running rGREAT")
-if(genome == "hg38" | genome == "mm10"){
+if(genome == "hg38" | genome == "hg19" | genome == "mm10" | genome == "mm9"){
   GREATjob <- sigRegions %>% 
     rGREAT::submitGreatJob(bg = regions,
                            species = genome,
@@ -560,7 +538,9 @@ blocks <- dmrseq::dmrseq(bs = bs.filtered,
                          minInSpan = 500,
                          bpSpan = 5e4,
                          maxGapSmooth = 1e6,
-                         maxGap = 5e3)
+                         maxGap = 5e3,
+                         BPPARAM = BiocParallel::MulticoreParam(workers = cores)
+                         )
 
 glue::glue("Selecting significant blocks...")
 
@@ -597,6 +577,31 @@ if(sum(blocks$pval < 0.05) > 0){
   dev.off()
 }
 
+glue::glue("Blocks timing...")
+end_time <- Sys.time()
+end_time - start_time
+
+
+# Annotate blocks with gene symbols ---------------------------------------
+
+if(sum(blocks$pval < 0.05) > 0){
+  glue::glue("Annotating blocks with gene symbols...")
+  sigBlocks %>%
+    annotateRegions(TxDb = TxDb,
+                    annoDb = annoDb) %T>%
+    DMReport(regions = blocks,
+             bsseq = bs.filtered.bsseq,
+             coverage = coverage,
+             name = "blockReport") %>% 
+    openxlsx::write.xlsx(file = "Blocks/Blocks_annotated.xlsx")
+}
+
+glue::glue("Annotating background blocks with gene symbols...")
+blocks %>%
+  annotateRegions(TxDb = TxDb,
+                  annoDb = annoDb) %>% 
+  openxlsx::write.xlsx(file = "Blocks/background_blocks_annotated.xlsx")
+
 glue::glue("Saving RData...")
 blocks_env <- ls(all = TRUE)[!(ls(all = TRUE) %in% bismark_env) &
                                !(ls(all = TRUE) %in% DMRs_env) &
@@ -604,10 +609,6 @@ blocks_env <- ls(all = TRUE)[!(ls(all = TRUE) %in% bismark_env) &
                                !(ls(all = TRUE) %in% GO_env)]
 save(list = blocks_env, file = "RData/Blocks.RData")
 #load("RData/Blocks.RData")
-
-glue::glue("Blocks timing...")
-end_time <- Sys.time()
-end_time - start_time
 
 # End ---------------------------------------------------------------------
 

NAMESPACE
@@ -4,10 +4,10 @@ export(CGiPCA)
 export(DMReport)
 export(GOfuncR)
 export(GOplot)
-export(GREAT)
 export(PCA)
 export(annotateCpGs)
 export(annotateGenic)
+export(annotateRegions)
 export(densityPlot)
 export(df2bed)
 export(getBackground)
@@ -16,17 +16,13 @@ export(gg_color_hue)
 export(globalStats)
 export(gr2bed)
 export(gr2csv)
-export(labelDirection)
 export(manQQ)
 export(methylLearn)
-export(packageLoad)
 export(plotDMRs2)
 export(processBismark)
 export(saveExternal)
 export(smooth2txt)
-export(smoothHeatmap)
 export(smoothPheatmap)
-export(tidyRegions)
 export(windowsPCA)
 import(BiocGenerics)
 import(Boruta)
@@ -45,14 +41,11 @@ import(dmrseq)
 import(enrichR)
 import(ggbiplot)
 import(ggplot2)
-import(gplots)
 import(gt)
-import(liftOver)
 import(lsmeans)
 import(optparse)
 import(pheatmap)
 import(rGREAT)
-import(rtracklayer)
 import(sigFeature)
 import(tidyverse)
 importFrom(RColorBrewer,brewer.pal)
@@ -62,4 +55,3 @@ importFrom(grDevices,rainbow)
 importFrom(graphics,arrows)
 importFrom(openxlsx,read.xlsx)
 importFrom(parallel,mclapply)
-importFrom(plyr,mapvalues)

R/GOplot.R
@@ -1,106 +1,0 @@
-#' GOplot
-#' @description Plots top signficant Gene Ontology and pathway terms from enrichR, rGREAT, and GOfuncR.
-#' @param GO A list of ontology and pathway data frames returned from \code{enrichR::enrichr()}, \code{rGREAT::getEnrichmentTables()}, or \code{GOfuncR::go_enrich()}.
-#' @param tool A character vector of the name of the database (enrichR, rGREAT, or GOfuncR).
-#' @return A \code{ggplot} object of top significant GO and pathway terms from an \code{enrichR} or \code{rGREAT} analysis.
-#'  that can be viewed by calling it, saved with \code{ggplot2::ggsave()}, or further modified by adding \code{ggplot2} syntax.
-#' @import tidyverse
-#' @import enrichR
-#' @import rGREAT
-#' @import GOfuncR
-#' @import ggplot2
-#' @export GOplot
-
-GOplot <- function(GO = GO,
-                   tool = NULL){
-  
-  if(tool == "enrichR"){
-    GOplot <- rbind(GO$GO_Biological_Process_2018[c(1:5),],
-                    GO$GO_Cellular_Component_2018[c(1:5),],
-                    GO$GO_Molecular_Function_2018[c(1:5),],
-                    GO$KEGG_2019_Human[c(1:5),]
-                    ) %>%
-      dplyr::as_tibble() %>%
-      cbind(
-        dplyr::as_tibble(
-          c(
-            rep("Biological Process", 5),
-            rep("Cellular Component", 5),
-            rep("Molecular Function", 5),
-            rep("KEGG", 5)
-          )
-        )
-      ) %>%
-      dplyr::select(Term, P.value, value, Combined.Score) %>%
-      dplyr::filter(P.value <= 0.05) %>%
-      dplyr::mutate(P.value = -log10(P.value)) %>%
-      dplyr::rename(`-log10.p-value` = P.value) %>%
-      dplyr::rename(Database = value) %>% 
-      dplyr::mutate(Database = stringr::str_replace(.$Database, "KEGG", "Pathway (KEGG)")) %>% 
-      dplyr::mutate(Term = stringr::str_replace(.$Term, "\\(.*", "")) %>%
-      dplyr::mutate(Term = stringr::str_replace(.$Term, "_.*", ""))
-  
-  }else if(tool == "rGREAT"){
-    GOplot <- rbind(GO$`GO Biological Process`[c(1:5),],
-                    GO$`GO Cellular Component`[c(1:5),],
-                    GO$`GO Molecular Function`[c(1:5),]
-                    ) %>%
-      dplyr::as_tibble() %>%
-      cbind(
-        dplyr::as_tibble(
-          c(
-            rep("Biological Process", 5),
-            rep("Cellular Component", 5),
-            rep("Molecular Function", 5)
-          )
-        )
-      ) %>%
-      dplyr::select(name, Hyper_Raw_PValue, value) %>%
-      dplyr::filter(Hyper_Raw_PValue <= 0.05) %>%
-      dplyr::mutate(Hyper_Raw_PValue = -log10(Hyper_Raw_PValue)) %>%
-      dplyr::rename(`-log10.p-value`= Hyper_Raw_PValue,
-                    Database = value,
-                    Term = name) 
-  
-  }else if(tool == "GOfuncR"){
-    GOplot <- rbind(GO$results %>% dplyr::filter(ontology == "biological_process") %>% dplyr::slice(1:5),
-                    GO$results %>% dplyr::filter(ontology == "cellular_component") %>% dplyr::slice(1:5),
-                    GO$results %>% dplyr::filter(ontology == "molecular_function") %>% dplyr::slice(1:5)
-                    ) %>%
-      dplyr::as_tibble() %>%
-      cbind(
-        dplyr::as_tibble(
-          c(
-            rep("Biological Process", 5),
-            rep("Cellular Component", 5),
-            rep("Molecular Function", 5)
-          )
-        )
-      ) %>%
-      dplyr::select(node_name, raw_p_overrep, value) %>%
-      dplyr::filter(raw_p_overrep <= 0.05) %>%
-      dplyr::mutate(raw_p_overrep = -log10(raw_p_overrep)) %>%
-      dplyr::rename(`-log10.p-value`= raw_p_overrep,
-                    Database = value,
-                    Term = node_name) 
-   
-  }else{
-    stop(glue("{tool} is not supported, please choose either enrichR, rGREAT, or GOfuncR [Case Sensitive]"))
-  }
-   
-  GOplot <- GOplot %>%
-    dplyr::mutate(Term = stringr::str_trim(.$Term)) %>%
-    dplyr::mutate(Term = stringr::str_to_title(.$Term)) %>%
-    dplyr::mutate(Term = stringr::str_wrap(.$Term, 70)) %>% 
-    dplyr::mutate(Database = factor(.$Database)) %>% 
-    dplyr::mutate(Term = factor(.$Term, levels = unique(.$Term[order(forcats::fct_rev(.$Database), .$`-log10.p-value`)]))) %>% 
-    ggplot2::ggplot(aes(x = Term, y = `-log10.p-value`, fill = Database, group = Database)) +
-    geom_bar(stat = "identity", position = position_dodge(), color = "Black") +
-    coord_flip() +
-    scale_y_continuous(expand = c(0, 0)) +
-    labs(y = expression("-log"[10](p))) +
-    theme_classic() +
-    theme(axis.text = element_text(size = 14),
-          axis.title.y = element_blank()) %>%
-    return()
-}

R/GREAT.R
@@ -1,59 +1,0 @@
-#' GREAT
-#' @description Note: Depracted by GREAT version 4.0 update. Perform gene ontology of DMRs relative to background regions from \code{dmrseq::dmrseq()} using \code{rGREAT}.
-#' @param siRegions A \code{GRanges} object of signficant DMRs returned by \code{dmrseq:dmrseq()}
-#' @param regions A \code{GRanges} object of background regions returned by \code{dmrseq:dmrseq()}
-#' @param genome A character vector specifying the genome of interest ("hg38" or "mm10")
-#' @return A list of gene ontology enrichments.
-#' @import rGREAT
-#' @import liftOver
-#' @import rtracklayer
-#' @importFrom glue glue
-#' @export GREAT
-GREAT <- function(sigRegions = sigRegions,
-                  regions = regions, 
-                  genome = genome){
-  if(genome == "hg38"){
-    cat("\n[DMRichR] Obtaining liftOver information for GREAT \t", format(Sys.time(), "%d-%m-%Y %X"))
-    path <- system.file(package="liftOver", "extdata", "hg38ToHg19.over.chain")
-    ch <- import.chain(path)
-    ch
-    
-    glue::glue("liftOver DMRs...")
-    seqlevelsStyle(sigRegions) <- "UCSC"
-    sigRegions_liftOver <- liftOver(sigRegions, ch)
-    class(sigRegions_liftOver)
-    sigRegions_liftOver <- unlist(sigRegions_liftOver)
-    #stopifnot(length(sigRegions) > length(sigRegions_liftOver))
-    
-    glue::glue("liftOver background regions...")
-    seqlevelsStyle(regions) <- "UCSC"
-    regions_liftOver <- liftOver(regions, ch)
-    class(regions_liftOver)
-    regions_liftOver <- unlist(regions_liftOver)
-    #stopifnot(length(regions) > length(regions_liftOver))
-  }
-  
-  if(genome == "hg38" | genome == "mm10"){
-    cat("\n[DMRichR] Submitting to GREAT\ \t\t\t\t", format(Sys.time(), "%d-%m-%Y %X"), "\n")
-    if(genome == "hg38"){
-      gr <- sigRegions_liftOver
-      bg <- regions_liftOver
-      species <- "hg19"
-    }else if(genome == "mm10"){
-      gr <- sigRegions
-      bg <- regions
-      species <- "mm10"
-    }else{
-      stop(paste(genome, "is not suppourted for GREAT, please choose either hg38 or mm10 [Case Sensitive]"))
-    }
-    
-    job <- submitGreatJob(gr,
-                          bg = bg,
-                          species = species,
-                          request_interval = 1)
-    job
-    #availableCategories(job)
-    #availableOntologies(job)
-  }
-  return(job)
-}

commit 3e4095f8f89057e09d8524f481773c1f6873ccbc

Fix for manQQ() bug that was introduced in ad8ce9a159168bd3a97468e1374ae013a4c1e1f8

DESCRIPTION
@@ -1,6 +1,6 @@
 Package: DMRichR
 Title: Enrich your DMR analysis
-Version: 1.2.8
+Version: 1.2.9
 Authors@R: c(
   person("Ben", "Laufer", email = "blaufer@ucdavis.edu", role = c("aut", "cre")),
   person("Charles", "Mordaunt", email = "cemordaunt@ucdavis.edu", role = "ctb"),

R/helperFunctions.R
@@ -139,10 +139,10 @@ manQQ <- function(backgroundAnno = backgroundAnno,
   glue::glue("Generating Manhattan and QQ plots...")
   setwd("DMRs")
   CMplot::CMplot(backgroundAnno %>%
-                   ChIPseeker::as.GRanges() %>%
+                   GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = TRUE) %>%
                    sort() %>%
-                   as.data.frame() %>%
-                   dplyr::select(SYMBOL, seqnames, start, pval) %>%
+                   dplyr::as_tibble() %>%
+                   dplyr::select(geneSymbol, seqnames, start, p.value) %>%
                    dplyr::mutate(seqnames = substring(.$seqnames, 4)),
                  col = DMRichR::gg_color_hue(2),
                  plot.type = c("m","q"),

exec/DM.R
@@ -539,8 +539,10 @@ regions %>%
   
 # Manhattan and Q-Q plots -------------------------------------------------
 
-regionsAnno %>%
-  manQQ()
+regions %>%
+    annotateRegions(TxDb = TxDb,
+                    annoDb = annoDb) %>% 
+    manQQ()
 
 # Gene Ontology analyses --------------------------------------------------
 